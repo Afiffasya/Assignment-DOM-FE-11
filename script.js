355a4d27b41847f03b369e269b24d20caef7dbe2
Afiffasya <afiffasya18@gmail.com>
2023-01-17 07:21:08 +0700

    add all file

---
+++ script.js
@@ -0,0 +1,18 @@
+function changeButtonDesc() {
+    let btnElement = document.getElementById("btnnn");
+    btnElement.innerHTML  = (btnElement.innerHTML == "Log in") ? "Log out" : "Log in";
+}
+
+function myFunction() {
+    alert("Page is loaded");
+}
+
+function bigImg(x) {
+    x.style.height = "60px";
+    x.style.width = "60px";
+}
+  
+function normalImg(x) {
+    x.style.height = "40px";
+    x.style.width = "40px";
+}
